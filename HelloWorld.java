8d8758dcd241d8c3edb307f71a0e34aa7441df3c
Charlio <chalioxumykj>
2020-10-16 22:16:08 -0700

    added a print in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! I have programmed in java for 3 years.");
+        System.out.println("The original Charlio has joined the project!");
     }
 }
