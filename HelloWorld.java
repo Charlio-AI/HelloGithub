00a14b1037fef39d18f4fbf6dbbe672610590e04
Charlio <chalioxumykj>
2020-10-16 21:52:46 -0700

    added experience of java in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! I have programmed in java for 3 years.");
     }
 }
